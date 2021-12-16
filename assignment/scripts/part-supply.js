56386b0e45ab2e5a80b4416cad2f9572e06a2289
Tony LaForgia <alforgia428@gmail.com>
2021-12-16 14:14:46 -0600

    Completed WK 3 part 3 #8

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -59,7 +59,11 @@ for (let x of supplyChanges){
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
-
+let total = 0;
+for (let index = 0; index < supplyChanges.length; index++){
+  total = total + supplyChanges[index];
+}
+console.log("Total number of parts = " + total);
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
 //    There are 572 parts in total, and each box holds 7 parts.
